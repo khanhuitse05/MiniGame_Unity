a01ae73aa2255855da27624a576c56356796e18c
Tô Ngọc Khánh <khanhuitse05@gmail.com>
2017-07-31 11:58:24 +0700

    +SwirlyPipe

---
+++ swirly-pipe-game/Assets/SwirlyPipe/Materials/ObstacleMaterialsColor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMaterialsColor : MonoBehaviour {
+    public static readonly Color[] colors = { Color.blue, Color.red, Color.green, Color.yellow, Color.magenta };
+    private void OnEnable()
+    {
+        gameObject.GetComponent<Renderer>().material.color = colors[Random.Range(0, colors.Length)];
+    }
+}
